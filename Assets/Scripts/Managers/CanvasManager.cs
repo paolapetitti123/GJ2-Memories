93e55d00803b7fb03d80f612cc61cea588464040
allyjenna <allyjenna@me.com>
2023-11-25 16:24:34 -0500

    door controller

--- Assets/Scripts/Managers/CanvasManager.cs
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -1,53 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class CanvasManager : MonoBehaviour
-{
-    public List<Canvas> canvases;  // List of all canvases in the game
-    private Canvas currentCanvas;   // The currently active canvas
+//public class CanvasManager : MonoBehaviour
+//{
+//    public List<Canvas> canvases;  // List of all canvases in the game
+//    private Canvas currentCanvas;   // The currently active canvas
 
-    // Singleton instance for easy access
-    private static CanvasManager _instance;
-    public static CanvasManager Instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                Debug.LogError("CanvasManager is not initialized.");
-            }
-            return _instance;
-        }
-    }
+//    // Singleton instance for easy access
+//    private static CanvasManager _instance;
+//    public static CanvasManager Instance
+//    {
+//        get
+//        {
+//            if (_instance == null)
+//            {
+//                Debug.LogError("CanvasManager is not initialized.");
+//            }
+//            return _instance;
+//        }
+//    }
 
-    private void Awake()
-    {
-        // No need to check for _instance or use DontDestroyOnLoad
-        _instance = this;
-    }
+//    private void Awake()
+//    {
+//        // No need to check for _instance or use DontDestroyOnLoad
+//        _instance = this;
+//    }
 
-    void Start()
-    {
-        // Initialize the canvas manager with the first canvas
-        SwitchCanvas(canvases[0]);
-    }
+//    void Start()
+//    {
+//        // Initialize the canvas manager with the first canvas
+//        SwitchCanvas(canvases[0]);
+//    }
 
-    public void SwitchCanvas(Canvas newCanvas)
-    {
-        // Deactivate the current canvas
-        if (currentCanvas != null)
-        {
-            currentCanvas.gameObject.SetActive(false);
-        }
+//    public void SwitchCanvas(Canvas newCanvas)
+//    {
+//        // Deactivate the current canvas
+//        if (currentCanvas != null)
+//        {
+//            currentCanvas.gameObject.SetActive(false);
+//        }
 
-        // Activate the new canvas
-        newCanvas.gameObject.SetActive(true);
+//        // Activate the new canvas
+//        newCanvas.gameObject.SetActive(true);
 
-        // Update the current canvas reference
-        currentCanvas = newCanvas;
+//        // Update the current canvas reference
+//        currentCanvas = newCanvas;
 
-        // Log Canvas everytime it switches
-        Debug.Log("The current canvas is " + currentCanvas);
-    }
-}
+//        // Log Canvas everytime it switches
+//        Debug.Log("The current canvas is " + currentCanvas);
+//    }
+//}
--- Assets/Scripts/PlayerNavigation/DoorsController.cs
+++ Assets/Scripts/PlayerNavigation/DoorsController.cs
@@ -4,52 +4,109 @@ using UnityEngine;
 
 public class DoorsController : MonoBehaviour
 {
-    private bool playerInTriggerZone = false;
-    private float triggerCooldown = 1f;  // Adjust the cooldown time as needed
-    private float lastTriggerTime;
+    //private bool playerInTriggerZone = false;
+    //private float triggerCooldown = 1f;  // Adjust the cooldown time as needed
+    //private float lastTriggerTime;
+    public GameObject DoorOne;
+    public GameObject DoorTwo;
+    public GameObject DoorThree;
+    public GameObject DoorFour;
 
-    private void OnTriggerEnter2D(Collider2D other)
+
+
+    public GameObject outside;
+    public GameObject alchemist;
+    public GameObject kitchen;
+
+
+    private void Start()
     {
-        if (other.CompareTag("Player") && !playerInTriggerZone && Time.time - lastTriggerTime > triggerCooldown)
-        {
-            playerInTriggerZone = true;
-            lastTriggerTime = Time.time;
+        outside.SetActive(true);
+        alchemist.SetActive(false);
+        kitchen.SetActive(false);
 
-            // Player has entered the trigger zone and cooldown has passed
-            HandleDoorCollision();
-        }
+
     }
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player") && !playerInTriggerZone && Time.time - lastTriggerTime > triggerCooldown)
+    //    {
+    //        playerInTriggerZone = true;
+    //        lastTriggerTime = Time.time;
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            playerInTriggerZone = false;
-        }
-    }
+    //        // Player has entered the trigger zone and cooldown has passed
+    //        HandleDoorCollision();
+    //    }
+    //}
 
-    private void HandleDoorCollision()
+    //private void OnTriggerExit2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        playerInTriggerZone = false;
+    //    }
+    //}
+
+    public void OnTriggerEnter2D(Collider2D other)
     {
         // Differentiate between doors based on their tags
-        if (gameObject.CompareTag("DoorOne"))
+        if (other.gameObject == DoorOne)
         {
+            Debug.Log("hit door one");
             // Handle collision with DoorOne
-            CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[1]);
+            outside.SetActive(false);
+            kitchen.SetActive(false);
+            alchemist.SetActive(true);
+            // Assuming your player's GameObject has a Transform component (if not, adjust accordingly)
+            Transform playerTransform = gameObject.GetComponent<Transform>();
+
+            // Set the player's y-position to its negative value
+            playerTransform.position = new Vector3(playerTransform.position.x, -Mathf.Abs(playerTransform.position.y), playerTransform.position.z);
+
+            //CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[1]);
         }
-        else if (gameObject.CompareTag("DoorTwo"))
+        else if (other.gameObject == DoorTwo)
         {
-            // Handle collision with DoorTwo
-            CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[0]);
+            Debug.Log("hit door two");
+            // Handle collision with DoorOne
+            outside.SetActive(false);
+            kitchen.SetActive(true);
+            alchemist.SetActive(false);
+            Transform playerTransform = gameObject.GetComponent<Transform>();
+
+            // Set the player's y-position to its negative value
+            playerTransform.position = new Vector3(playerTransform.position.x, Mathf.Abs(playerTransform.position.y + 2f), playerTransform.position.z);
+
+
+            //CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[0]);
         }
-        else if (gameObject.CompareTag("DoorThree"))
+        else if (other.gameObject == DoorThree)
         {
-            // Handle collision with DoorTwo
-            CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[2]);
+            Debug.Log("hit door three");
+            // Handle collision with DoorOne
+            outside.SetActive(true);
+            kitchen.SetActive(false);
+            alchemist.SetActive(false);
+            Transform playerTransform = gameObject.GetComponent<Transform>();
+
+            // Set the player's y-position to its negative value
+            playerTransform.position = new Vector3(playerTransform.position.x, Mathf.Abs(playerTransform.position.y + 2f), playerTransform.position.z);
+
+            //CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[2]);
         }
-        else if (gameObject.CompareTag("DoorFour"))
+        else if (other.gameObject == DoorFour)
         {
-            // Handle collision with DoorTwo
-            CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[1]);
+            Debug.Log("hit door four");
+            // Handle collision with DoorOne
+            outside.SetActive(true);
+            kitchen.SetActive(false);
+            alchemist.SetActive(false);
+            Transform playerTransform = gameObject.GetComponent<Transform>();
+
+            // Set the player's y-position to its negative value
+            playerTransform.position = new Vector3(playerTransform.position.x, -Mathf.Abs(playerTransform.position.y -2f), playerTransform.position.z);
+
+            //CanvasManager.Instance.SwitchCanvas(CanvasManager.Instance.canvases[2]);
         }
         else if (gameObject.CompareTag("DoorFive"))
         {
