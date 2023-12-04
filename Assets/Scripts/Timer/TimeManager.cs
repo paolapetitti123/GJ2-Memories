bb49ce27cfe8d2e5349c409649379698352ff732
paolap1998 <paolaf0980@gmail.com>
2023-12-04 11:51:45 -0500

    pushing timer changes

---
+++ Assets/Scripts/Timer/TimeManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TimeManager : MonoBehaviour
+{
+    public static Action OnMinuteChanged;
+    public static Action OnHourChanged;
+
+    public static int Minute { get; private set; }
+    public static int Hour { get; private set; }
+
+    private float minuteToRealTime = 1f;
+    private float timer;
+
+    public GameObject guardConvo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Minute = 0;
+        Hour = 10;
+        timer = minuteToRealTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!guardConvo.activeInHierarchy)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                Minute++;
+                OnMinuteChanged?.Invoke();
+                if (Minute >= 60)
+                {
+                    Hour++;
+                    OnHourChanged?.Invoke();
+                    Minute = 0;
+                }
+                timer = minuteToRealTime;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Timer/TimerGameOver.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerGameOver : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        TimeManager.OnMinuteChanged += TimeCheck;
+        TimeManager.OnHourChanged += TimeCheck;
+    }
+
+    private void OnDisable()
+    {
+        TimeManager.OnMinuteChanged -= TimeCheck;
+        TimeManager.OnHourChanged -= TimeCheck;
+    }
+
+    private void TimeCheck()
+    {
+        if(TimeManager.Hour == 17 && TimeManager.Minute == 59)
+        {
+            GameStateManager.Instance.LoadScene(GameStateManager.Scene.GameLose);
+        }
+    }
+
+}
